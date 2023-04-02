07a92628b97b65005e30cead5d4e1c9e10db73ce
Emeey-Lanr <emeeylanr04@gmail.com>
2023-04-02 08:16:19 +0100

    worked on the index, signup, signi

---
+++ src/Components/Chat.tsx
@@ -0,0 +1,13 @@
+import ChattingSpace from "./ChattingSpace"
+import Sidebar from "./Sidebar"
+
+const Chat = () => {
+  return (
+    <div className="chat-div">
+      <ChattingSpace />
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Chat
